10fa0b7f97e0950500c2062edc5c97c4d1107ad4
Richard Grac <ricardodanielgarcianavarro@MacBook-Pro-de-Itexico-3.local>
2019-12-23 08:46:13 -0600

    Checking that getSecretWord call is being called doing it with a mocked function

--- src/App.js
+++ src/App.js
@@ -5,37 +5,38 @@ import GuessedWords from "./Components/GuessedWords/GuessedWords";
 import {connect} from "react-redux";
 import Input from "./Components/Input/input";
 import {bindActionCreators} from 'redux'
+import {getSecretWordAxios} from './redux/GuessedWords/actions'
 
-class App extends Component {
+export class App extends Component {
 
     componentDidMount() {
-
+        this.props.getSecretWordAxios()
     }
 
     render() {
+        const {success, guessedWords} = this.props
 
-    return (
-        <div className="App container">
-          <h1 className={'text-center'}>The App</h1>
-          <Congrats success={this.props.success} />
-          <Input />
-          <GuessedWords guessedWords={[
-            {guessedWord: 'train', letterMatchCount: 3},
-          ]}/>
-        </div>
-    )
-  }
+        return (
+            <div className="App container">
+                <h1 className={'text-center'}>The App</h1>
+                <Congrats success={success}/>
+                <Input/>
+                <GuessedWords guessedWords={guessedWords}/>
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = state => {
     return {
-        success: state.successReducer.success
+        success: state.successReducer.success,
+        guessedWords: state.guessedWordsReducer.guessedWords,
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return bindActionCreators({
-
+        getSecretWordAxios
     }, dispatch)
 }
 
