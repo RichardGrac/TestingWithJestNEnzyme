41f3d24fe241e0e0f77735e42c0aa2c186b8fe88
Ricardo Garcia <Richard_Grac@hotmail.com>
2019-12-24 09:16:59 -0600

    All tests deleted for this branch

--- src/redux/Success/index.test.js
+++
@@ -1,8 +0,0 @@
-import successReducer from "./reducers";
-
-describe('testing reducers', () => {
-    test('It returns default initial state with success equals false', () => {
-        const result = successReducer(undefined, {})
-        expect(result.success).toEqual(false)
-    })
-})
